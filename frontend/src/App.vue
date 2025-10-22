62d68e556d71ad3e17ad8556cff3c75a4643a3aa
ryanguit <ryan.guit@gmail.com>
2025-10-22 15:58:51 +0300

    add global style for App.vue for button style reuse

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,11 +8,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/make-poll">Create Poll</RouterLink>
       </nav>
     </div>
   </header>
@@ -20,7 +19,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <RouterView />
 </template>
 
-<style scoped>
+<style>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -29,6 +28,13 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+.button {
+  border: 2px white solid;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin: auto;
 }
 
 nav {
