d6b450e3e42f4d53b047e2021440d44995738a71
ryanguit <ryan.guit@gmail.com>
2025-10-22 22:50:59 +0300

    frontend: calls to query poll and votepoll

--- frontend/src/calls/calls.js
+++ frontend/src/calls/calls.js
@@ -1,5 +1,51 @@
 const api = "http://[::1]:5000/api";
 
+
+export const getPoll = async (pollId) => {
+  const queryURL = api + "/query-poll?poll=" + pollId;
+  return await new Promise( (resolve) => {
+        const newRequest = new XMLHttpRequest();
+        newRequest.open("GET", queryURL, true);
+        newRequest.responseType = "json";
+        // newRequest.setRequestHeader("Content-Type", "application/json")
+        newRequest.onload = (value) => {
+            if (value.target.status === 200) {
+                resolve(value.target.response);
+            } else {
+                resolve(false);
+            }
+        };
+        newRequest.onerror = () => {
+            resolve(false);
+        }
+        newRequest.send();
+    });
+}
+
+export const votePoll = async (pollId, key) => {
+  const voteURL = api + "/vote-poll";
+  return await new Promise( (resolve) => {
+        const newRequest = new XMLHttpRequest();
+        newRequest.open("POST", voteURL, true);
+        newRequest.responseType = "json";
+        newRequest.setRequestHeader("Content-Type", "application/json")
+        newRequest.onload = (value) => {
+            if (value.target.status === 200) {
+                resolve(value.target.response);
+            } else {
+                resolve(false);
+            }
+        };
+        newRequest.onerror = () => {
+            resolve(false);
+        }
+        newRequest.send(JSON.stringify({
+          poll_id: pollId,
+          key: key
+        }));
+    });
+}
+
 export const createPoll = async (formData) => {
   console.log(formData);
     //make request to backend to store this.
@@ -21,14 +67,6 @@ export const createPoll = async (formData) => {
         }
         newRequest.send(JSON.stringify(formData));
     });
-    // const response = await fetch(storeURL, {
-    //       method: "POST",
-    //       headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //       },
-    //       body: JSON.stringify(formData)
-    //     });
     console.log(response)
 }
 
