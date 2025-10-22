b7c80636acadae66245fd9f2587b7791d63ac4b3
ryanguit <ryan.guit@gmail.com>
2025-10-22 19:02:15 +0300

    frontend production build fix

--- frontend/src/components/MakePoll/MakePoll.vue
+++ frontend/src/components/MakePoll/MakePoll.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <div class="button" @click=createEmptyPoll()>Create a Poll</div>
+        <div class="button" @click="this?.createEmptyPoll()">Create a Poll</div>
         <!-- <Poll v-if=this.pollEditor editable />  -->
-        <div class="button" @click=this.publishPoll()>Publish Poll</div>
+        <div class="button" @click="this?.publishPoll()">Publish Poll</div>
     </div>    
 </template>
-<script src="./make-poll.js"/>
+<script src="./make-poll.js" />
--- frontend/src/views/MakePollView.vue
+++ frontend/src/views/MakePollView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// @ts-ignore
 import MakePoll from '../components/MakePoll/MakePoll.vue'
 </script>
 
