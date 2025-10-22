d02c354b8d045a6c717e536966e1d10932de56fa
ryanguit <ryan.guit@gmail.com>
2025-10-22 19:01:42 +0300

    frotend fetch api does not work with NestJS or something. Use XMLHttpRequest

--- frontend/src/calls/calls.js
+++ frontend/src/calls/calls.js
@@ -1,6 +1,35 @@
-export const createPoll = (formData) => {
+const api = process.env.API_URL ?? "http://[::1]:5000/api";
+
+export const createPoll = async (formData) => {
+  console.log(formData);
     //make request to backend to store this.
-    console.log(formData)
+    const storeURL = api + '/store-poll';
+    const response = await new Promise( (resolve) => {
+        const newRequest = new XMLHttpRequest();
+        newRequest.open("POST", storeURL, true);
+        newRequest.responseType = "json";
+        newRequest.setRequestHeader("Content-Type", "application/json")
+        newRequest.onload = (value) => {
+            if (value.target.status === 200) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        };
+        newRequest.onerror = () => {
+            resolve(false);
+        }
+        newRequest.send(JSON.stringify(formData));
+    });
+    // const response = await fetch(storeURL, {
+    //       method: "POST",
+    //       headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //       },
+    //       body: JSON.stringify(formData)
+    //     });
+    console.log(response)
 }
 
 export const getPollResults = (pollId) => {
