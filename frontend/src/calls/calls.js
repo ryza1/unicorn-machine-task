37a6f3855a3da53ca6bafa3154148872997fbccc
ryanguit <ryan.guit@gmail.com>
2025-10-22 15:59:22 +0300

    wip functions for making requests to backend

---
+++ frontend/src/calls/calls.js
@@ -0,0 +1,8 @@
+export const createPoll = (formData) => {
+    //make request to backend to store this.
+    console.log(formData)
+}
+
+export const getPollResults = (pollId) => {
+    //retrieve poll results 
+}
