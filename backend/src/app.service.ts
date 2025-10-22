1da4bb10f94297ebbefab650bda1255e7129a506
ryanguit <ryan.guit@gmail.com>
2025-10-22 19:17:48 +0300

    backend: Store the polloptions with votes

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Options } from '@nestjs/common';
+
+interface PollOption {
+  option: string,
+  votes: number
+} 
+
+
 
 interface PollInterface {
-  title: string, options: string[]
+  title: string,
+  options: Record<number, PollOption>,
+  locked: boolean,
 }
 
 let fakeDatabaseJson: Record<string, PollInterface> = {};
@@ -13,11 +22,24 @@ export class AppService {
   storePoll(data: {title: string, options: string[]}): string {
     //validation needed
     console.log("stored", data);
+    let options = {};
+    data.options.forEach((item: string, index: number) => {
+      options[index.toString()] = {
+        option: item,
+        votes: 0
+      }
+    });
+    const newPoll = {
+      title: data.title,
+      options: options,
+      locked: false
+    }
     //generate the key and link. use the link queryparam as key to JSONDB
     //organisation needed as I don't want to oop through an array.
     const newField = data?.title.split(" ").join('') ?? "randomString";
-    fakeDatabaseJson[newField] = data;
+    fakeDatabaseJson[newField] = newPoll;
     console.log(fakeDatabaseJson);
+    console.log(JSON.stringify(fakeDatabaseJson[newField]));
     return data?.title;
   }
 }
