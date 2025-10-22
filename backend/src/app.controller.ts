6c1841170c445e6698804f6ac411b1e531214e70
ryanguit <ryan.guit@gmail.com>
2025-10-22 22:50:12 +0300

    backend: endpoints for query polls and vote polls

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body} from '@nestjs/common';
+import { Controller, Get, Post, Body, Req} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,9 +10,19 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Get('query-poll')
+  getPoll(@Req() req: any): any {
+    return this.appService.getPoll(req.query?.poll)
+  }
+
   @Post('store-poll')
   async storePoll(@Body() body: {title: string, options: string[]}) {
     return this.appService.storePoll((body));
+  }
+
+  @Post('vote-poll')
+  votePoll(@Body() body: {poll_id: string, key: number}) {
+    return this.appService.votePoll((body));
   }
 
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -19,9 +19,21 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+
+  votePoll(data: {poll_id: string, key: number}): string {
+    console.log(fakeDatabaseJson, fakeDatabaseJson[data.poll_id]);
+    fakeDatabaseJson[data.poll_id].options[data.key].votes = fakeDatabaseJson[data.poll_id].options[data.key].votes + 1;
+
+    console.log(fakeDatabaseJson[data.poll_id]);
+
+    return data.poll_id;
+  }
+
+
   storePoll(data: {title: string, options: string[]}): string {
     //validation needed
-    console.log("stored", data);
+    
     let options = {};
     data.options.forEach((item: string, index: number) => {
       options[index.toString()] = {
@@ -36,10 +48,15 @@ export class AppService {
     }
     //generate the key and link. use the link queryparam as key to JSONDB
     //organisation needed as I don't want to oop through an array.
-    const newField = data?.title.split(" ").join('') ?? "randomString";
+    const newField = (data?.title.split(" ").join(''))?.toLowerCase() ?? "randomString";
     fakeDatabaseJson[newField] = newPoll;
     console.log(fakeDatabaseJson);
     console.log(JSON.stringify(fakeDatabaseJson[newField]));
     return data?.title;
+  }
+  getPoll(poll_id: number): PollInterface {
+    const result = fakeDatabaseJson[poll_id];
+    console.log("rsult", result)
+    return result;
   }
 }
